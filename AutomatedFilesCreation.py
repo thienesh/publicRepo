29a55c73ac211699493c7e33ee5639c4f19fcd9f
thienesh <thienesh8@gmail.com>
2023-04-03 11:25:19 +0530

    Automated script for 10 python files creation

--- AutomatedFilesCreation.py
+++ AutomatedFilesCreation.py
@@ -6,6 +6,6 @@ if not os.path.exists(directory):
     os.makedirs(directory)
 
 for i in range(1,11):
-    file_path = os.path.join(directory,f"python program {i}")
+    file_path = os.path.join(directory,f"python program {i}.py")
     with open(file_path,'w') as f:
         f.write(f"This file is created for python program no.{i}")
