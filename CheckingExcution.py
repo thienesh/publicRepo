e4792fe4d5f58faba0e3287fa90fe8f812ad64f2
thienesh <thienesh8@gmail.com>
2023-04-26 19:02:48 +0530

    Checking Executed or not

---
+++ CheckingExcution.py
@@ -0,0 +1,19 @@
+﻿
+import subprocess
+
+cmd = "wmic logicaldisk get deviceid, freespace, size"
+
+result = subprocess.run(cmd,shell=True)
+
+if result.returncode == 0:
+    print("Executed")
+else:
+    print("Not executed")
+
+# print(f"output : {result}")
+# print(f"output is : {result.returncode}")
+
+
+
+# print(f"output : {result}")
+# print(f"output from popen : {len(result.readlines())}")
