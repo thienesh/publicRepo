ab9b7d9cac5a29209ef3f1ddf06b4fa66d68f205
thienesh <thienesh8@gmail.com>
2023-04-26 11:25:22 +0530

    Script to connect amazon site for fast and medium users[prototype]

--- scriptforServerConnection.py
+++ scriptforServerConnection.py
@@ -1,5 +1,7 @@
 import os
 import time
+from selenium import webdriver
+
 u1 = ["rama", "ram", "krish"]
 u2 = ["sama", "sam", "sudo"]
 
@@ -12,15 +14,33 @@ user = input("Enter the name:")
 if user in u1:
     print("Allow to better performing server1:", s1)
     os.system(cmd1)
+    driver = webdriver.Edge()
+    driver.get("http://www.amazon.com")
+    driver.fullscreen_window()
+    time.sleep(7)
+    print("Successfully connected to amazon server!!")
+    driver.close()
 
 elif user in u2:
     time.sleep(0.5)
-    for i in range(1,5):
+    for i in range(1, 5):
         time.sleep(2)
         print(f"time taken:{i} seconds")
     print("Connection Established...Allow to medium performing server2:", s2)
     os.system(cmd2)
+    driver = webdriver.Edge()
+    driver.get("http://www.amazon.com")
+    driver.fullscreen_window()
+    time.sleep(7)
+    print("Successfully connected to amazon server!!")
+    driver.close()
 
 else:
     print("Allow to better performing server1:", s1)
     os.system(cmd1)
+    driver = webdriver.Edge()
+    driver.get("http://www.amazon.com")
+    driver.fullscreen_window()
+    time.sleep(7)
+    print("Successfully connected to amazon server!!")
+    driver.close()
