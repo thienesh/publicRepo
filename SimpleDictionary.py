ab8610901921898ffd1b44acf181193a56a464d7
thienesh <thienesh8@gmail.com>
2023-05-05 19:31:41 +0530

    simple dictionary

--- SimpleDictionary.py
+++ SimpleDictionary.py
@@ -1,4 +1,4 @@
-dic1 = {"name":"ana","city":"blr"}
+dic1 = {"name": "ana", "city": "blr"}
 
 keys = dic1.keys()
 values = dic1.values()
@@ -6,7 +6,7 @@ values = dic1.values()
 print(keys)
 print(values)
 
-#print(dic1)
-dic1.update({"city":"New York"})
+# print(dic1)
+dic1.update({"city": "New York"})
 
 print(dic1)
