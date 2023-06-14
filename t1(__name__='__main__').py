8090a58ac59fdee5a9050a12cdd18704a2ece257
Admin <thienesh8@gmail.com>
2023-06-14 18:53:27 +0530

    Creating a function and importing in the another file(without excuting the previous program)

---
+++ t1(__name__='__main__').py
@@ -0,0 +1,14 @@
+#Addition of two numbers:
+def sum(a, b):
+    sum1 = a + b
+    return sum1
+def mul(a, b):
+    mul1 = a * b
+    return mul1
+
+def main():
+    result = sum(5, 5)
+    print(result)
+
+if __name__ == "__main__":
+    main()
---
+++ t2(CallingFunctionIn(t1).py
@@ -0,0 +1,9 @@
+import t1
+a = int(input("num1= "))
+b = int(input("num2= "))
+
+#print(dir(t1))
+result = t1.sum(a, b)
+print("Addition of given numbers:: ", result)
+rs2 = t1.mul(a, b)
+print("Multiplication of given numbers:: ", rs2)
